4cf696257d638150b7b61446d92472e84545c28a
larry0161 <50087930+larry0161@users.noreply.github.com>
2022-05-20 16:38:50 -0700

    Update 23&MeGenomeAnalyzer.py

--- 23&MeGenomeAnalyzer.py
+++ 23&MeGenomeAnalyzer.py
@@ -20,20 +20,20 @@ import shutil
 # Make sure your raw data is in the same directory as this program
 file = 'YOUR_23&ME_RAW_DATA.txt'
 data = open(file,'r').readlines()
-n = len(data)
+data_length = len(data)
 cpu=os.cpu_count()
 
 
 @ray.remote
-def f(data_split,file_num):
+def searchNCBI(data_split,file_num):
     temp_file = open('file'+str(file_num),'w')
     
     server = "http://grch37.rest.ensembl.org/variation/human/"
-    ext = "?phenotypes=1"
+    endpoint = "?phenotypes=1"
     for i in data_split:
         rsid,chrome,position,alleles =i.split()
 
-        r = requests.get(server+rsid+ext, headers={ "Content-Type" : "application/json"})
+        r = requests.get(server+rsid+endpoint, headers={ "Content-Type" : "application/json"})
         gene = 'N/A'
         most_severe = 'N/A'
         detected = 'N/A'
@@ -58,12 +58,12 @@ def f(data_split,file_num):
 
 
 
-step = n//cpu
-steps = [i for i in range(0,n-step+1,step)]
-steps.append(n)
+step = data_length//cpu
+steps = [i for i in range(0,data_length-step+1,step)]
+steps.append(data_length)
 
 def go():
-    ray.get([f.remote(data[steps[i]:steps[i+1]],i) for i in range(cpu)])
+    ray.get([searchNCBI.remote(data[steps[i]:steps[i+1]],i) for i in range(cpu)])
 go()
 #AnalyzedGenome.txt includes results from all 600k+ RSID genotypes, including those with no associated traits or RSID's not found in the database
 with open('AnalyzedGenome.txt','wb') as wfd:
@@ -76,10 +76,10 @@ with open('AnalyzedGenome.txt','wb') as wfd:
 
 #CleanedAnalysis.txt only includes results where RSID and user genotype were documented with an associated phenotype
 AnalyzedGenome = open('AnalyzedGenome.txt','r').readlines()
-n=len(AnalyzedGenome)
+AG_length=len(AnalyzedGenome)
 CleanedAnalysis = open("CleanedAnalysis.txt",'w')
 CleanedAnalysis.write(("RSID"+'\t'+"GENE"+'\t'+"POSSIBLE_TRAITS"+'\n'))
-for i in range(1,n):
+for i in range(1,AG_length):
     row = AnalyzedGenome[i].split()
     if row[-1]!='N/A':
         CleanedAnalysis.write(yo[i])
